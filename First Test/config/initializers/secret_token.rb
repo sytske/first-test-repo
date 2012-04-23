253a85be1fbfb92501e19d56b4652851514cb23a
Sander Wind <sander@sanderwind.nl>
2012-04-23 14:21:01 +0200

    First Ruby on Rails entry
    Shoop da WHOOP!

---
+++ First Test/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstTest::Application.config.secret_token = '8f51809362875a58db46475c6c271545fe9bb31852b67276875d24517956d4b9b7b9c22c923fb6ae284c2bfd75b85f21a6271e898cc5e640b39d10e7c390fde2'
